335df1af0e341cc834a989ed274a858b088f3313
moxith001 <moxith001@gmail.com>
2025-12-07 21:06:56 +0530

    commit message

---
+++ task1.py
@@ -0,0 +1,14 @@
+
+a = input("Enter the firt number: ")
+b=  input("Enter the second number: ")
+
+add= int(a) + int(b)
+sub= int(a) - int(b)
+mul= int(a) * int(b)
+div= int(a) / int(b)
+
+print("Addition: ", add)
+print("Subtraction: ", sub)
+print("Multiplication: ", mul)
+print("Division: ", div)
+
---
+++ task2.py
@@ -0,0 +1,5 @@
+
+first_name = input("Enter first Name: ")
+last_name = input("Enter last name: ")
+
+print("Hello, " + first_name + " " + last_name+ "!", "Welcome to the Python program.");
